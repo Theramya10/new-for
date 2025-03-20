d58b93c59274aa4b7372c2eee12d0c49ffde686a
theramya10 <sahooramya@gmail.com>
2025-03-20 08:05:31 +0530

    5th commit

--- s3.tf
+++ s3.tf
@@ -8,15 +8,3 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
-
-resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
-  bucket = aws_s3_bucket.website_bucket.id
-  rule {
-    id = "move-to-glacier"
-    status = "Enabled"
-    transition {
-      days          = 30
-      storage_class = "GLACIER"
-    }
-  }
-}
