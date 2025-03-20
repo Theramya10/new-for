9a19dd5a07443b948090393a8b90347c3cf30a25
theramya10 <sahooramya@gmail.com>
2025-03-20 10:58:12 +0530

    16th commit

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,7 +1,8 @@
+# Create CloudFront Distribution
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
-    domain_name = aws_s3_bucket.theramya10.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.theramya10.id}"
+    domain_name = data.aws_s3_bucket.existing_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${data.aws_s3_bucket.existing_bucket.id}"
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
@@ -14,7 +15,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${aws_s3_bucket.theramya10.id}"
+    target_origin_id = "S3-${data.aws_s3_bucket.existing_bucket.id}"
 
     forwarded_values {
       query_string = false
@@ -33,6 +34,6 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true  # Default SSL certificate
+    cloudfront_default_certificate = true  # Using default SSL certificate
   }
 }
--- iam.tf
+++ iam.tf
@@ -1,19 +1,25 @@
-resource "aws_cloudfront_origin_access_identity" "oai" {
-  comment = "OAI for S3 bucket"
+# Fetch existing S3 bucket
+data "aws_s3_bucket" "existing_bucket" {
+  bucket = "theramya10"
 }
 
+# Create CloudFront Origin Access Identity (OAI)
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for S3 bucket theramya10"
+}
+
+# S3 Bucket Policy to Allow CloudFront Access
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.theramya10.id
+  bucket = data.aws_s3_bucket.existing_bucket.id
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect = "Allow",
-        Principal = {
-          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
-        },
-        Action   = "s3:GetObject",
-        Resource = "${aws_s3_bucket.theramya10.arn}/*"
+        Effect    = "Allow",
+        Principal = { AWS = aws_cloudfront_origin_access_identity.oai.iam_arn },
+        Action    = "s3:GetObject",
+        Resource  = "${data.aws_s3_bucket.existing_bucket.arn}/*"
       }
     ]
   })
