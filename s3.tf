5e15575a4c353e7bd54fea97e58564d64ca31eaa
theramya10 <sahooramya@gmail.com>
2025-03-20 10:17:16 +0530

    14th commit

--- s3.tf
+++ s3.tf
@@ -1,7 +1,3 @@
-resource "aws_s3_bucket" "theramya10" {
-  bucket = "theramya10"
-}
-
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.theramya10.id
   versioning_configuration {
